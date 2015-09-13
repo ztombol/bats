e59924453a65c3c22abfeeb2fb158e165ecff6de
Thomas LEVEIL <thomasleveil@gmail.com>
2015-09-13 13:02:36 +0000

    assert_ouput: now also print the command that was run

--- lib/bats/batslib.bash
+++ lib/bats/batslib.bash
@@ -291,6 +291,7 @@ assert_output() {
     else
       if [[ $output != "$expected" ]]; then
         batslib_print_kv_single_or_multi 8 \
+            'command' "$cmd" \
             'expected' "$expected" \
             'actual'   "$output" \
           | batslib_decorate 'output differs' \
